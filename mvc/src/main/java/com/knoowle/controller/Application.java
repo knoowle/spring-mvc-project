9fa09dfecb92e8a0f01b7620efbde3e996ab4cf1
knoowle <shunzheheliu@yeah.net>
2013-01-08 22:22:00 +0800

    Revert "呵呵"
    This reverts commit ae177e8f080788073b53ceb6a2182b40b628dc95.

--- mvc/src/main/java/com/knoowle/controller/Application.java
+++ mvc/src/main/java/com/knoowle/controller/Application.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
